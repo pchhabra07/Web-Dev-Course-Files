ea3bfe1f830b2e8037a422570b1741cfcf9facec
“[Pratham <[chhabrapratham2005@gmail.com]>
2025-04-05 12:07:33 +0530

    Lecture 8 Files

---
+++ Lec8_/2_JS_Basics/script.js
@@ -0,0 +1,8 @@
+//Once a variable is declared with a type (let,,var or const), it cannot be 
+// re-declared but if no type is given, then a type is given further, that is okay.
+
+x=10;
+console.log(x);
+
+var x=12;
+console.log(x);
