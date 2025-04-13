3ba5d5e7b017177b1cc72607a0dba03cf42ac401
“[Pratham <[chhabrapratham2005@gmail.com]>
2025-04-14 02:12:05 +0530

    Updated files Lecture 11

--- Lec11_Callbacks_Promises_Binding/2_promises/4_promiseAll.js
+++ Lec11_Callbacks_Promises_Binding/2_promises/4_promiseAll.js
@@ -13,7 +13,7 @@ let p1=new Promise((res,rej)=>{
 
 let p2=new Promise((res,rej)=>{
     console.log("Data set 2 encryption started...");
-    let dataEncryptionFinished=true;
+    let dataEncryptionFinished=false;
     setTimeout(()=>{
         if(dataEncryptionFinished){
             res("Data set 2 encryption is finished now...");
@@ -24,8 +24,6 @@ let p2=new Promise((res,rej)=>{
     },3000)
 })
 
-
-//goes to .then and .catch on the basis of which ever promise's execution finished first.
 // Promise.race([p1,p2])
 //     .then((msg)=>{
 //         console.log(msg);
@@ -34,13 +32,10 @@ let p2=new Promise((res,rej)=>{
 //         console.log(err);
 //     })
 
-
-//waits for finishing of execution of all promises and gives message of all promises in an array if all are resolved.
-//if even one is rejected, it goes to catch for that one.
-Promise.all([p1,p2])
-    .then((msg)=>{
-        console.log(msg);
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
+// Promise.all([p1,p2])
+//     .then((msg)=>{
+//         console.log(msg);
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//     })
